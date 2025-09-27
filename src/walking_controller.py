70baa94ca4107e186a4ca7153bdd6916aae5e59b
Romain Desarzens <hib.pub@protonmail.com>
2025-09-27 10:01:03 +0200

    Add model initialization

--- src/walking_controller.py
+++ src/walking_controller.py
@@ -5,7 +5,14 @@ from matplotlib import pyplot as plt
 from shapely import Polygon, Point, affinity, union
 from shapely.ops import nearest_points
 from scipy.linalg import solve_discrete_are
+import os, sys
+import numpy as np
+import pinocchio as pin
+from time import sleep
+from pinocchio.visualize import MeshcatVisualizer
+from qpsolvers import solve_qp
 
+from scipy.spatial.transform import Rotation
 
 def compute_zmp_ref(t, com_initial_pose, steps, ss_t, ds_t):
     T = len(t)
@@ -340,6 +347,95 @@ if __name__ == "__main__":
 
     update_frequency = 0.02
     draw_every = max(1, int(update_frequency / dt))
+
+
+    def set_joint(q, joint_name, val):
+        jid = full_model.getJointId(joint_name)
+        if jid > 0 and full_model.joints[jid].nq == 1:
+            q[full_model.joints[jid].idx_q] = val
+
+
+    # Solver params
+    ITERS = 200
+    TOL_DQ = 1e-8
+    W_COM = 3.0
+    W_TORSO = 10.0  # tune
+    MU = 1e-6
+    DQ_LIM = 0.5
+    RFREF = pin.ReferenceFrame.LOCAL_WORLD_ALIGNED
+
+    # Load full model
+    PKG_PARENT = os.path.expanduser(os.environ.get("PKG_PARENT", "~/projects"))
+    URDF = os.path.join(PKG_PARENT, "talos_data/urdf/talos_full.urdf")
+
+    if not os.path.isfile(URDF):
+        print(f"URDF not found: {URDF}\nSet PKG_PARENT or clone talos_data.", file=sys.stderr)
+        sys.exit(1)
+
+    full_model, full_col_model, full_vis_model = pin.buildModelsFromUrdf(URDF, PKG_PARENT, pin.JointModelFreeFlyer())
+
+    # List joints
+    for j_id, j_name in enumerate(full_model.names):
+        print(j_id, j_name, full_model.joints[j_id].shortname())
+
+    # List frames
+    for i, frame in enumerate(full_model.frames):
+        print(i, frame.name, frame.parent, frame.type)
+
+    # Initialize the model position
+    q = pin.neutral(full_model)
+
+    # Position the arms
+    set_joint(q, "leg_left_4_joint", 0.0)
+    set_joint(q, "leg_right_4_joint", 0.0)
+    set_joint(q, "arm_right_4_joint", -1.2)
+    set_joint(q, "arm_left_4_joint", -1.2)
+
+    # We lock joints of the upper body
+    joints_to_lock = [i for i in range(14, 48)]
+
+    # Build reduced model
+    red_model, red_geom = pin.buildReducedModel(full_model, full_col_model, joints_to_lock, q)
+    _, red_vis = pin.buildReducedModel(full_model, full_vis_model, joints_to_lock, q)
+    red_data = red_model.createData()
+
+    # Initialize visualizer
+    viz = MeshcatVisualizer(red_model, red_geom, red_vis)
+    try:
+        viz.initViewer(open=True)
+        viz.loadViewerModel()
+    except Exception:
+        viz = None
+
+    LF = red_model.getFrameId("left_sole_link")
+    RF = red_model.getFrameId("right_sole_link")
+    TORSO = red_model.getFrameId("torso_1_link")
+
+    # Initialize reduced model
+    q = pin.neutral(red_model)
+
+    # Initialize legs position
+    set_joint(q, "leg_left_1_joint", 0.0)
+    set_joint(q, "leg_left_2_joint", 0.0)
+    set_joint(q, "leg_left_3_joint", -0.5)
+    set_joint(q, "leg_left_4_joint", 1.0)
+    set_joint(q, "leg_left_5_joint", -0.6)
+
+    set_joint(q, "leg_right_1_joint", 0.0)
+    set_joint(q, "leg_right_2_joint", 0.0)
+    set_joint(q, "leg_right_3_joint", -0.5)
+    set_joint(q, "leg_right_4_joint", 1.0)
+    set_joint(q, "leg_right_5_joint", -0.6)
+
+    pin.forwardKinematics(red_model, red_data, q)
+    pin.updateFramePlacements(red_model, red_data)
+    oMf_rf0 = red_data.oMf[RF].copy()
+    oMf_lf0 = red_data.oMf[LF].copy()
+
+    print(f"Initial center of mass position: {pin.centerOfMass(red_model, red_data, q)}")
+    print(f"Initial left foot position: {red_data.oMf[LF].translation}")
+
+    sleep(2.0)
 
     frames = []
     for k in range(T):
