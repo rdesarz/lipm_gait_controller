321edb4ec530f14c0ad84bf70019f16482e130de
Romain Desarzens <hib.pub@protonmail.com>
2025-10-02 14:08:35 +0200

    Refactor preview control to use methods in module

--- examples/lipm_preview_control.py
+++ examples/lipm_preview_control.py
@@ -2,96 +2,10 @@ import math
 
 import numpy as np
 from matplotlib import pyplot as plt
-import imageio.v2 as imageio
-from shapely import Polygon, Point, affinity, union
-from shapely.ops import nearest_points
-from scipy.linalg import solve_discrete_are
+from shapely import Polygon
 
-
-def compute_zmp_ref(com_initial_pose, steps, dt, ss_t, ds_t):
-    T = int((len(steps) - 1) * (ss_t + ds_t) / dt + (ds_t + ss_t) / dt)
-
-    t = np.arange(T) * dt
-    zmp_ref = np.zeros([T, 2])
-
-    # Step on the first foot
-    mask = t < ds_t
-    alpha = t[mask] / ds_t
-    zmp_ref[mask, :] = (1 - alpha)[:, None] * com_initial_pose + alpha[:, None] * steps[0]
-
-    # Alternate between foot
-    for idx, (current_step, next_step) in enumerate(zip(steps[:-1], steps[1:])):
-        # Compute current time range
-        t_start = ds_t + idx * (ss_t + ds_t)
-
-        # Add single support phase
-        zmp_ref[(t >= t_start) & (t < t_start + ss_t)] = current_step
-
-        # Add double support phase
-        mask = (t >= t_start + ss_t) & (t < t_start + ss_t + ds_t)
-        alpha = (t[mask] - (t_start + ss_t)) / ds_t
-        zmp_ref[mask, :] = (1 - alpha)[:, None] * current_step + alpha[:, None] * next_step
-
-    # Last phase is single support at last foot pose
-    mask = t >= ds_t + (len(steps) - 1) * (ss_t + ds_t)
-    zmp_ref[mask, :] = steps[-1]
-
-    return t, zmp_ref
-
-
-def compute_double_support_polygon(current_foot_pose, next_foot_pose, foot_shape):
-    curent_foot = affinity.translate(foot_shape, xoff=current_foot_pose[0], yoff=current_foot_pose[1])
-    next_foot = affinity.translate(foot_shape, xoff=next_foot_pose[0], yoff=next_foot_pose[1])
-
-    return union(curent_foot, next_foot).convex_hull
-
-
-def compute_single_support_polygon(current_foot_pose, foot_shape):
-    return affinity.translate(foot_shape, xoff=current_foot_pose[0], yoff=current_foot_pose[1])
-
-
-def plot_steps(axes, steps_pose, step_shape):
-    # Plot double support polygon
-    for current_step, next_step in zip(steps_pose[:-1], steps_pose[1:]):
-        support_polygon = compute_double_support_polygon(current_step, next_step, step_shape)
-
-        x, y = support_polygon.exterior.xy
-        axes.plot(x, y, color="blue")  # outline
-        axes.fill(x, y, color="lightblue", alpha=0.5)  # filled polygon
-
-    # Plot single support polygon
-    for current_step in steps_pose:
-        support_polygon = compute_single_support_polygon(current_step, step_shape)
-
-        x, y = support_polygon.exterior.xy
-        axes.plot(x, y, color="red")  # outline
-        axes.fill(x, y, color="red", alpha=0.5)  # filled polygon
-
-
-def get_active_polygon(k, dt, steps_pose, t_ss, t_ds, foot_shape):
-    tk = k * dt
-    t_step = t_ss + t_ds
-    i = int(tk / t_step)
-    i = min(i, len(steps_pose) - 2)
-    t_in = tk - i * t_step
-    if t_in < t_ss:
-        return compute_single_support_polygon(steps_pose[i], foot_shape)
-    elif tk >= (len(steps_pose) - 1) * t_step:
-        return compute_single_support_polygon(steps_pose[-1], foot_shape)
-    else:
-        return compute_double_support_polygon(steps_pose[i], steps_pose[i + 1], foot_shape)
-
-
-def clamp_to_polygon(u_xy, poly: Polygon):
-    p = Point(u_xy[0], u_xy[1])
-    if poly.contains(p):
-        return u_xy
-
-    # nearest point on boundary
-    q = nearest_points(poly.exterior, p)[0]
-
-    return np.array([q.x, q.y])
-
+from lipm_walking_controller.controller import compute_zmp_ref, initialize_preview_control
+from lipm_walking_controller.foot import get_active_polygon, compute_feet_path_and_poses
 
 if __name__ == "__main__":
     # Parameters
@@ -111,12 +25,13 @@ if __name__ == "__main__":
     # ZMP reference parameters
     t_ss = 1.0  # Single support phase time window
     t_ds = 0.2  # Double support phase time window
-    com_initial_pose = np.array([0.0, 0.0])
     foot_shape = Polygon(((0.11, 0.05), (0.11, -0.05), (-0.11, -0.05), (-0.11, 0.05)))
-    steps_pose = np.array([[0.0, -0.1],
-                           [0.3, 0.1],
-                           [0.6, -0.1],
-                           [0.9, 0.1]])
+    n_steps = 5
+    l_stride = 0.3
+    max_height_foot = 0.05
+    com_initial_pose = np.array([0.0, 0.0])
+    rf_initial_pose = np.array([0.0, -0.1, 0.0])
+    lf_initial_pose = np.array([0.0, 0.1, 0.0])
 
     # Applied force parameters
     m = 60.0  # kg
@@ -126,42 +41,15 @@ if __name__ == "__main__":
     k_push = int((t_ss + 0.5 * t_ds) / dt)  # mid-DS of first pair
 
     # Build ZMP reference to track
-    t, zmp_ref = compute_zmp_ref(com_initial_pose, steps_pose, dt, t_ss, t_ds)
+    t, lf_path, rf_path, steps_pose, phases = compute_feet_path_and_poses(rf_initial_pose, lf_initial_pose, n_steps,
+                                                                          t_ss, t_ds,
+                                                                          l_stride, dt, max_height_foot)
 
-    # Discrete cart-table model with jerk input
-    A = np.array([[1.0, dt, 0.5 * dt * dt],
-                  [0.0, 1.0, dt],
-                  [0.0, 0.0, 1.0]], dtype=float)
-    B = np.array([[dt ** 3 / 6.0],
-                  [dt ** 2 / 2.0],
-                  [dt]], dtype=float)
-    C = np.array([[1.0, 0.0, -zc / g]], dtype=float)  # 1x3
+    # Build ZMP reference to track
+    zmp_ref = compute_zmp_ref(t, com_initial_pose, steps_pose, t_ss, t_ds)
 
-    A1 = np.block([[np.eye(1), C @ A],
-                   [np.zeros((3, 1)), A]])  # 4x4
-    B1 = np.vstack((C @ B, B))  # 4x1
-    I1 = np.vstack((np.array([1]), np.zeros((3, 1))))  # 4x1
-    F = np.vstack((C @ A, A))
-
-    Q = np.block([[Qe, np.zeros((1, 3))],
-                  [np.zeros((3, 1)), Qx]])  # 4x4
-
-    # Compute K by solving Ricatti equation
-    K = solve_discrete_are(A1, B1, Q, R)
-
-    # Compute Gi and Gx
-    Gi = float(np.linalg.inv(R + B1.T @ K @ B1) @ B1.T @ K @ I1)
-    Gx = np.linalg.inv(R + B1.T @ K @ B1) @ B1.T @ K @ F
-
-    # Compute Gd
-    Ac = A1 - B1 @ np.linalg.inv(R + B1.T @ K @ B1) @ B1.T @ K @ A1
-    X1 = Ac.T @ K @ I1
-    X = X1
-    Gd = np.zeros((n_preview_steps - 1))
-    Gd[0] = -Gi
-    for l in range(n_preview_steps - 2):
-        Gd[l + 1] = np.linalg.inv(R + B1.T @ K @ B1) @ B1.T @ X
-        X = Ac.T @ X
+    # Initialize controller
+    A, B, C, Gd, Gx, Gi = initialize_preview_control(dt, zc, g, Qe, Qx, R, n_preview_steps)
 
     T = len(t)
     u = np.zeros((T, 2))
@@ -171,8 +59,8 @@ if __name__ == "__main__":
         np.repeat(zmp_ref[-1][None, :], n_preview_steps, axis=0)
     ])
 
-    x0 = np.array([0.0, 0.0, 0.0, 0.0], dtype=float)
-    y0 = np.array([0.0, 0.0, 0.0, 0.0], dtype=float)
+    x0 = np.array([0.0, com_initial_pose[0], 0.0, 0.0], dtype=float)
+    y0 = np.array([0.0, com_initial_pose[1], 0.0, 0.0], dtype=float)
     x = np.zeros((len(zmp_ref) + 1, 4), dtype=float)
     y = np.zeros((len(zmp_ref) + 1, 4), dtype=float)
     x[0] = x0
